d9e6ba088892627953d0d4552afa067d403344a0
Cesar Perez <Cesar.Perez5272@yahoo.com>
2020-01-03 18:43:07 -0500

    'pushed'

--- src/folders/FolderContainer.js
+++ src/folders/FolderContainer.js
@@ -1,12 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 class FolderContainer extends React.Component {
+    constructor(){
+        super()
+        this.sayHello = this.sayHello.bind(this)
+    }
+
+    sayHello = () => {
+        alert('working')
+    }
     render() {
         return (
             <div className='folder_list'>
                 <ul>
-                    <li>Folder One</li>
-                    <li>Folder Two</li>
+                    <li onClick= {this.sayHello}>Folder One</li>
+                    <li onClick= {this.sayHello}>Folder Two</li>
                     <li>Folder Three</li>
                     <li>Add a Folder</li>
                 </ul>
